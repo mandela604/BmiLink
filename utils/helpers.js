289f31a5ea9953fa38370e651c92475a535761b8
mandela604 <mandelaidiahi@gmail.com>
2026-05-27 06:32:06 -0700

    code updatedn

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,20 @@
+// utils/helpers.js
 const crypto = require('crypto');
 
 const hashIP = (ip) => {
-  return crypto.createHash('sha256')
-    .update(ip + (process.env.IP_SALT || 'storelink'))
-    .digest('hex')
-    .slice(0, 16);
+  if (!ip) return null;
+  
+  try {
+    return crypto.createHash('sha256')
+      .update(ip + (process.env.IP_SALT || 'storelink'))
+      .digest('hex')
+      .slice(0, 16);
+  } catch (e) {
+    console.error('hashIP failed:', e);
+    return null;
+  }
+};
+
+module.exports = {
+  hashIP
 };
